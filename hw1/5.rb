7913c466ff72e2f8370d5ac9229ee5fb40c10505
kristinh <kristin@precis-project.com>
2012-02-28 09:10:10 -0800

    exercise 5 complete

--- hw1/5.rb
+++ hw1/5.rb
@@ -3,8 +3,14 @@ class Class
     attr_name = attr_name.to_s  # make sure it's a string
     attr_reader attr_name  # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-    class_eval %{
-
+    class_eval %Q{
+	def #{attr_name}=(value)
+		@#{attr_name} = value
+		if @#{attr_name}_history.nil?
+			@#{attr_name}_history = [nil]
+		end
+		@#{attr_name}_history.push(value)
+	end
     }
   end
 end
@@ -13,7 +19,13 @@ class Foo
   attr_accessor_with_history :bar
 end
 
+=begin
 f = Foo.new
 f.bar = 1
 f.bar = 2
-f.bar_history # => if your code works, should be [nil,1,2]
+f.bar
+puts f.bar_history.inspect # => if your code works, should be [nil,1,2]
+f = Foo.new
+f.bar = 4
+puts f.bar_history.inspect
+=end
