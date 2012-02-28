81e1086b70a327fdc5b55d4d30272357e4fbd7c0
kristinh <kristinh@precis-project.com>
2012-02-28 23:01:33 +0100

    fixed formatting

--- hw1/5.rb
+++ hw1/5.rb
@@ -4,13 +4,13 @@ class Class
     attr_reader attr_name  # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-	def #{attr_name}=(value)
-		@#{attr_name} = value
-		if @#{attr_name}_history.nil?
-			@#{attr_name}_history = [nil]
-		end
-		@#{attr_name}_history.push(value)
-	end
+      def #{attr_name}=(value)
+        @#{attr_name} = value
+		    if @#{attr_name}_history.nil?
+          @#{attr_name}_history = [nil]
+		    end
+		    @#{attr_name}_history.push(value)
+	    end
     }
   end
 end
