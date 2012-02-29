1087b079666f57fa3b97f182144c8bde4d20af57
kristinh <kristin@precis-project.com>
2012-02-28 20:39:03 -0800

    .

---
+++ hw2/1.rb
@@ -0,0 +1,52 @@
+class Numeric
+  @@currencies = {'dollar' => 1.000, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+  def method_missing(method_id, *args)
+    singular_currency = method_id.to_s.gsub( /s$/, '')
+    if @@currencies.has_key?(singular_currency)
+      @dollars = self * @@currencies[singular_currency]
+      self
+    elsif method_id.to_s == 'in'
+      singular_currency = args[0].to_s.gsub( /s$/,'')
+      @dollars / @@currencies[singular_currency]
+    else
+      super
+    end
+  end
+end
+
+class String
+  def palindrome?
+    forward_string = self.gsub(/\W/, '').downcase
+    reverse_string = forward_string.reverse
+    forward_string == reverse_string
+  end
+end
+
+module Enumerable
+  def palindrome?
+     if self.respond_to?("reverse")
+       forward = self.reverse
+       forward == self
+     else
+       false
+     end
+  end
+end
+
+=begin
+puts "Madam, I'm Adam!".palindrome?.inspect
+puts "Madamx, I'm Adam!".palindrome?.inspect
+t = {"hello"=>"world"}
+puts t.palindrome?.inspect
+puts [1,2,3,2,1].palindrome?.inspect
+puts [12,3,4].palindrome?.inspect
+
+puts 2.rupee.in(:dollar)
+puts 3.yen.in(:dollar)
+puts 6.euro.in(:dollar)
+puts 2.rupees.in(:dollars)
+puts 3.yen.in(:dollars)
+puts 6.euros.in(:dollars)
+puts 5.rupees.in(:yen)
+=end
+
