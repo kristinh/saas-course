21164f363577d3735efc943a16c49965cbc435a2
kristinh <kristinh@precis-project.com>
2012-03-06 22:51:34 +0100

    removed puts

--- hw2/1.rb
+++ hw2/1.rb
@@ -1,7 +1,6 @@
 class Numeric
   @@currencies = {'dollar' => 1.000, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
   def method_missing(method_id, *args)
-    puts method_id.to_s
     if method_id.to_s == 'in'
       singular_currency = args[0].to_s.gsub( /s$/, '')
       if @@currencies.has_key?(singular_currency)
