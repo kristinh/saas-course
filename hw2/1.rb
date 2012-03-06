4f76b64bacddf9b4fee6ec06de2e541bddfcd439
kristinh <kristinh@precis-project.com>
2012-03-06 22:47:56 +0100

    working, but ugly

--- hw2/1.rb
+++ hw2/1.rb
@@ -1,30 +1,23 @@
-class NoCurrencyConversion < StandardError
-end
-
 class Numeric
   @@currencies = {'dollar' => 1.000, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
   def method_missing(method_id, *args)
+    puts method_id.to_s
     if method_id.to_s == 'in'
-        convert(args[0])
-    else
-      begin
-        convert(method_id)
-      rescue NoCurrencyConversion
+      singular_currency = args[0].to_s.gsub( /s$/, '')
+      if @@currencies.has_key?(singular_currency)
+        self * @@currencies[singular_currency] / @@currencies[@currency]
+      else
         super
       end
-    end  
-  end
-
-  private
-  def convert(currency)
-    singular_currency = currency.to_s.gsub( /s$/, '')
-    if @@currencies.has_key?(singular_currency)
-      self * @@currencies[singular_currency]
     else
-      raise NoCurrencyConversion
+      singular_currency = method_id.to_s.gsub( /s$/, '')
+      if @@currencies.has_key?(singular_currency)
+        @currency = singular_currency
+        self
+      else 
+        super
+      end
     end
   end
-
 end
 
-puts 5.euros.in(:rupees).inspect
