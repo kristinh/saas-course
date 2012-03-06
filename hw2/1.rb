8a098d2e7cbdbb12e9c68021d449cc1c70e02400
kristinh <kristinh@precis-project.com>
2012-03-06 20:50:57 +0100

    Totally wrong attempt at 1

---
+++ hw2/1.rb
@@ -0,0 +1,22 @@
+class Numeric
+  @@currencies = {'dollar' => 1.000, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+  def method_missing(method_id)
+    singular_currency = method_id.to_s.gsub( /s$/, '')
+    if @@currencies.has_key?(singular_currency)
+      self
+    else
+      super
+    end
+  end
+
+  def in(currency)
+    singular_currency = currency.to_s.gsub( /s$/, '')
+    if @@currencies.has_key?(singular_currency)
+      self * @@currencies[singular_currency]
+    else
+      super
+    end
+  end
+end
+
+puts 5.euros.in(:rupees).inspect
